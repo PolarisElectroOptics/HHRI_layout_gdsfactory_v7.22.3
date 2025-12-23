b1b18ab6e7c60273034cf648b4b0f954fc137372
Polaris Electro-Optics <accounts@polariseo.com>
2025-12-22 23:53:38 -0800

    Floor plan with LS-MZM & frame

--- layout_die_HHRI.py
+++ layout_die_HHRI.py
@@ -8,31 +8,67 @@ from library_electrode_unified import *
 from library_electrode_params import *
 import pandas as pd
 
-combined_params = {**differential_electrode_params, **balun_sipho_params,  "MT1_from_PS": False, "PS_taper": True, "DC_MT1": False, "RF_out": True,
-                   "PS_length": 850,
-                   "trans_length": 200,
-                    "trans_length_to_term": 200,
+combined_params = {**differential_electrode_params, **balun_sipho_params, "gsgsg_variant": "DC", "MT1_from_PS": False, "PS_trans_length": 250, "PS_taper": True, "DC_MT1": False, "s2s_type": "adiabatic",  #MMI, adiabatic, power
+                    "w_slot": 0.20,
+                    "w_slotWG": 0.20,
+                    "S2S_ADIA_W": 0.4,
+                    "S2S_ADIA_W0": 0.18,
+                    "S2S_ADIA_R": 0.2, #default to w_slotWG
+                    "S2S_ADIA_S": 0.17, #default to w_slot
+                    "S2S_ADIA_G": 0.17,
+                    "S2S_ADIA_L1": 4,
+                    "S2S_ADIA_L2": 4,
+                    "S2S_ADIA_L3": 42,
+                    "S2S_ADIA_B1": 2.25,
+                    "S2S_ADIA_B2": 0.87,
+                    "S2S_ADIA_B3": 5,
+                    "S2S_ADIA_C1": 0.525,
+                    "S2S_ADIA_C2": 0.13,
+
+                    "pad_center_gnd_width": 80,
+                    "pad_inner_gap_width": 20,
+                    "pad_sig_width": 80,
+                    # "pad_outer_gap_width": 20,
+                    # "pad_outer_gnd_width": 80,  # 200-7.5,
+                    "pad_length": 100,
+
+                    "S2S_center_gnd_width": 50,
+                    "S2S_inner_gap_width": 16,
+                    "S2S_sig_width": 40,
+                    "S2S_outer_gap_width": 5,
+                    "S2S_outer_gnd_width": 60,
+                    "S2S_length": 50,
+
+                    "PS_center_gnd_width": 50,
+                    "PS_inner_gap_width": 16,
+                    "PS_sig_width": 40,
+                    "PS_outer_gap_width": 15, #used for GSGSG only
+                    "PS_outer_gnd_width": 50,#-40, #used for GSGSG only
+
                     "PS_trans_length": 100,
-                   "htr_bridge_place": True,
-                   "htr_bridge_width": 4,
-                   "htr_bridge_pitch": 70,
-                   "htr_bridge_reps": 3,
-                   }
-# temp fixes and offsets
-combined_params["wsclDOEcell_placeGC"] = 1
-# myXoff = 350
-myXoff = 70
-myYoff = 1230
+
+                    "pad_t_length": 60,
+            }
+combined_params["PS_length"] = 1000
 
 # here the cell building starts
-c = gf.Component("Mirach_DOE_1_2025_12_19_v7")
+c = gf.Component("TOP")
+
+
+
+myXoff = 150
+myYoff = 600
+
+
 
 # read and place scribe lanes
-scribeLane = gf.import_gds("DOEdieFrame.gds", with_metadata=True)
+scribeLane = gf.import_gds("HHRIdieFrame.gds", with_metadata=True)
 c << scribeLane
 
 # read DOE file (firs row has parameter names, other rows have values)
-myDOETable = pd.read_csv("DOE1_pars.csv")
+myDOETable = pd.read_csv("LSMZM_pars.csv")
+
+
 
 # assign parameters and create cells
 for j in range(len(myDOETable)):
@@ -43,14 +79,17 @@ for j in range(len(myDOETable)):
             combined_params[par] = myDOETable.iat[j,i]
         #print(par)
     # place DOE elements
-    mycell=wsclDOEcell(combined_params)
+    mycell=MZM_SilTerra(combined_params)
     _ = c << mycell
+    _.rotate(-90)
     # read placement coordinates and move
     X, Y = combined_params["X"], combined_params["Y"]
     _.move((X+myXoff, Y+myYoff))
 
 # c = merge_clad(c, 0)
 # c.name = "Mirach_DOE_1_2025_12_14_v2"
+
+
 c.show()
 
 
